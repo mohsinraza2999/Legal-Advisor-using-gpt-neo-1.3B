f488c168038e39343cebd59113206ef90d1854b6
mohsinraza2999 <mohsinraza2999@gmail.com>
2025-07-20 10:25:40 -0700

    all file added to the git

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from typing import List
+
+setup(
+    name="Legal Documentation Generation System",
+    version="0.0.1",
+    auther="Mohsin Raza",
+    author_email="mohsinraza2999@gmail.com"
+)
